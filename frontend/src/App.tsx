b320f75bb9e11b3c2042b402be7642a1defdc1cb
w0rm29 <adi.maheshwari98@gmail.com>
2024-11-22 00:15:58 -0500

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form  from './components/Form';
+import Submit from './components/Submit';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Chronoplan</h1>
+      {/* <Form/> */}
+      <Submit/>
+    </div>
+  );
+}
+
+export default App;
